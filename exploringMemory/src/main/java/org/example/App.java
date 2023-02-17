2a3448c204b404951b2a1bf99899d6baaf917c81
cristiii <cristian.vlad10@gmail.com>
2023-02-17 11:47:49 +0200

    exploring memory

---
+++ exploringMemory/src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int localValue = 5;
+        // pass by value - localValue var will not be changed
+        calculate(localValue);
+        System.out.println(localValue);
+
+        Customer c = new Customer("Sally");
+        renameCustomer(c);
+        System.out.println(c.getName());
+    }
+
+    public static void calculate(int calcValue) {
+        calcValue = calcValue * 100;
+    }
+
+    public static void renameCustomer(Customer cust) {
+        cust.setName("Diane");
+    }
+}
